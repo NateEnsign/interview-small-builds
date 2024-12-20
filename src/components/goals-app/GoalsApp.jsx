cdeef7ca0f1d61f6e30247d18902bfc18bf6a2fa
Nate Ensign <dixienate@gmail.com>
2024-12-20 08:07:31 -0700

    added state handling to goals app to update goals list in UI

--- src/components/goals-app/GoalsApp.jsx
+++ src/components/goals-app/GoalsApp.jsx
@@ -1,25 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import GoalList from "./GoalList";
 import NewGoal from "./NewGoal";
 import "./GoalsApp.css";
 
 const GoalsApp = () => {
-  const goals = [
+  const [courseGoals, setCourseGoals] = useState([
     { id: 'cg1', text: "Finish the course" },
     { id: 'cg2', text: "Help other people to learn along the way" },
     { id: 'cg3', text: "Go on to change the world" },
-  ];
+  ]);
 
   const addNewGoalHandler = (newGoal) => {
-    goals.push(newGoal);
-    console.log(goals)
+    setCourseGoals(courseGoals.concat(newGoal))
   }
 
   return (
     <div>
       <h1 className="goals-header">Goal List</h1>
       <NewGoal onAddGoal={addNewGoalHandler} />
-      <GoalList goals={goals} />
+      <GoalList goals={courseGoals} />
     </div>
   );
 };
