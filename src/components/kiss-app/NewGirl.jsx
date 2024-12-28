b80d610d4f3f235cf81236be20a893dec6d5e1e7
Nate Ensign <dixienate@gmail.com>
2024-12-28 09:19:15 -0700

    added functionality to add girl dynamically via text input

--- src/components/kiss-app/NewGirl.jsx
+++ src/components/kiss-app/NewGirl.jsx
@@ -1,21 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './NewGirl.css';
 
 const NewGirl = ({addGirl}) => {
+    const [textInput,setTextInput] = useState('');
+
     const addNewGirlHandler = (event) => {
         event.preventDefault();
 
         const newGirl = {
             id: Math.random().toString(),
-            name: 'Megan Fox',
+            name: textInput,
         }
+
+        setTextInput('');
         addGirl(newGirl);
+    }
+
+    const handleTextChange = (event) => {
+        setTextInput(event.target.value);
     }
 
   return (
     <div>
         <form className='new-girl' onSubmit={addNewGirlHandler}>
-            <input type="text" />
+            <input type="text" value={textInput} onChange={handleTextChange} />
             <button type='submit'>Add Girl</button>
         </form>
     </div>
