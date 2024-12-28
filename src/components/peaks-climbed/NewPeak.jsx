02ca0e1246b99c80ba37d4dc4f517d15e8b3c5f2
Nate Ensign <dixienate@gmail.com>
2024-12-28 09:12:50 -0700

    added functionality to dynamically add a peak I have climbed via text input

--- src/components/peaks-climbed/NewPeak.jsx
+++ src/components/peaks-climbed/NewPeak.jsx
@@ -1,21 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './NewPeak.css';
 
 const NewPeak = ({addPeak}) => {
+    const [textInput,setTextInput] = useState('');
+
     const onAddNewPeak = (event) => {
         event.preventDefault();
 
         const newPeak = {
             id: Math.random().toString(),
-            peak: 'Mount Everrest',
+            peak: textInput,
         }
+        setTextInput('');
         addPeak(newPeak)
+    }
+
+    const handleTextChange = (event) => {
+        setTextInput(event.target.value)
     }
 
   return (
     <div>
         <form className='peak-form' onSubmit={onAddNewPeak}>
-            <input type="text" />
+            <input type="text" value={textInput} onChange={handleTextChange} />
             <button type='submit'>Add Peak</button>
         </form>
     </div>
