10a851ec10868724a8f184e76a9c46019276a312
Nate Ensign <dixienate@gmail.com>
2024-12-28 09:09:26 -0700

    added functionality to dynamically add country from text input

--- src/components/travel-app/NewPlace.jsx
+++ src/components/travel-app/NewPlace.jsx
@@ -1,22 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './NewPlace.css'
 
 const NewPlace = ({addCountry}) => {
+    const [textInput,setTextInput] = useState('');
+
     const onAddPlace = (event) => {
         event.preventDefault();
 
     const newCountry = {
         id: Math.random().toString(),
-        name: 'Costa Rica',
+        name: textInput,
     }
 
+    setTextInput('');
+
     addCountry(newCountry)
+    }
+
+    const handleTextChange = (event) => {
+        setTextInput(event.target.value);
     }
 
   return (
     <div>
         <form className='place-form' onSubmit={onAddPlace}>
-            <input type="text" />
+            <input type="text" value={textInput} onChange={handleTextChange} />
             <button type='submit'>Add Country</button>
         </form>
     </div>
