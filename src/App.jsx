67e586975f77511c91f48b60ce4e16efd833e814
Nate Ensign <dixienate@gmail.com>
2025-01-11 09:02:46 -0700

    created booksread and booklist components

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,18 @@ import "./App.css";
 import GoalsApp from "./components/goals-app/GoalsApp";
 import ToDo from "./components/to-do-list/toDo";
 import GirlsKissed from "./components/kiss-app/GirlsKissed";
-import BooksRead from "./components/books-app/BooksRead";
 import PlacesVisited from "./components/countries-app/PlacesVisited";
 import PeaksClimbed from "./components/peaks-app/PeaksClimbed";
+import BooksRead from "./components/books-app/BooksRead";
 
 function App() {
   return (
     <>
+      <BooksRead />
       <PeaksClimbed />
       <GirlsKissed />
       <PlacesVisited />
       <GoalsApp />
-      <BooksRead />
       <ToDo />
     </>
   );
---
+++ src/components/books-app/BooksRead.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './BooksRead.css';
+
+const BooksRead = () => {
+  return (
+    <>
+    <h1 className='books-header'>Books I have read</h1>
+    </>
+  )
+}
+
+export default BooksRead
