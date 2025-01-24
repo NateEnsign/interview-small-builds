b124e18b194360bde579ee40b16daccb4653bc79
Nate Ensign <dixienate@gmail.com>
2025-01-24 16:35:36 -0700

    added function to handle new movie inputs

--- src/components/movie-app/NewMovie.jsx
+++ src/components/movie-app/NewMovie.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import './NewMovie.css';
 
 const NewMovie = () => {
+    const [textInput, setTextInput] = useState('')
+
+    const inputHandler = (event) => {
+        setTextInput(event.targe.value)
+    }
+
   return (
     <form className='movie-form'>
-        <input type="text" />
+        <input type="text" value={textInput} onChange={inputHandler} />
        <button>Add Movie</button>
     </form>
   )
