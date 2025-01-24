9ebace288d2bdf173a60e97ee0c6df9697b6a94e
Nate Ensign <dixienate@gmail.com>
2025-01-24 16:11:53 -0700

    created a topMovies component and added routes to navigate to it

--- src/components/movie-app/TopMovies.jsx
+++ src/components/movie-app/TopMovies.jsx
@@ -1,31 +1,8 @@
-import React, {useState} from 'react';
-
-import NewMovie from './NewMovie';
-import MovieList from './MovieList';
-import './TopMovies.css';
+import React from 'react'
 
 const TopMovies = () => {
-    const [movieData, setMovieData] = useState([
-        {id: 'm1', title: 'Hacksaw Ridge'},
-        {id: 'm2', title: 'Remember the Titans'},
-        {id: 'm3', title: 'About Time'},
-        {id: 'm4', title: 'Get Smart'}
-    ])
-
-    const handleDeleteMovie = (movieId) => {
-        setMovieData(prevMovieData => prevMovieData.filter(movie => movie.id !== movieId))
-    }
-
-    const handleAddMovie = (newMovie) => {
-        setMovieData(prevMovieData => [...prevMovieData, newMovie])
-    }
-
   return (
-    <div>
-        <h1 className='movie-header'>Favorite Movies I have Seen</h1>
-        <NewMovie addMovie={handleAddMovie} />
-        <MovieList movies={movieData} deleteMovie={handleDeleteMovie} />
-    </div>
+    <div>TopMovies</div>
   )
 }
 
