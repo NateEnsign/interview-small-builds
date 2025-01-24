b2330352c7eb2f522e44d31dea445fd91d3608c5
Nate Ensign <dixienate@gmail.com>
2025-01-24 16:28:45 -0700

    added styling for the movie list

--- src/components/movie-app/MovieList.jsx
+++ src/components/movie-app/MovieList.jsx
@@ -5,7 +5,7 @@ import "./MovieList.css";
 const MovieList = ({ movies }) => {
   return (
     <div>
-      <ul className="movies-list">
+      <ul className="movie-list">
         {movies.map(({ id, title }) => {
           return (
             <li key={id}>
