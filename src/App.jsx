4fe0df095373793bfbe29bb66365c86a787f3fe2
Nate Ensign <dixienate@gmail.com>
2025-01-27 19:01:38 -0700

    added a Chores component/page with routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import "./App.css";
 import GoalsApp from "./components/goals-app/GoalsApp";
-import ToDo from "./components/to-do-list/toDo";
 import PlacesVisited from "./components/countries-app/PlacesVisited";
 import PeaksClimbed from "./components/peaks-app/PeaksClimbed";
 import BooksRead from "./components/book-app/BooksRead";
 import TopMovies from "./components/movie-app/TopMovies";
 import LandingPage from "./pages/LandingPage";
+import Chores from "./components/toDo-app/Chores";
 import Header from "./components/Header";
 
 function App() {
@@ -22,7 +22,7 @@ function App() {
         <Route path="/peaks" element={<PeaksClimbed />} />
         <Route path="/places" element={<PlacesVisited />} />
         <Route path="/goals" element={<GoalsApp />} />
-        <Route path="/chores" element={<ToDo />} />
+        <Route path='/chores' element={<Chores />} />
       </Routes>
     </Router>
   );
---
+++ src/components/toDo-app/Chores.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './Chores.css';
+
+const Chores = () => {
+  return (
+    <div>
+        <h1 className='chores-header'>To do List</h1>
+    </div>
+  )
+}
+
+export default Chores
